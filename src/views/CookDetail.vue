940011648a49eefe4c64b159f888fb2e84d49517
cx <2283286272@qq.com>
2024-07-01 10:44:49 +0800

    补充菜谱详情评论区

--- src/views/CookDetail.vue
+++ src/views/CookDetail.vue
@@ -2,11 +2,12 @@
  * @Author: ChenXin
  * @Date: 2024-06-27 10:32:17
  * @LastEditors: ChenXin
- * @LastEditTime: 2024-06-30 17:07:58
+ * @LastEditTime: 2024-07-01 10:42:45
  * @FilePath: CookDetail.vue
  * @Description: For learning only
 -->
 <script setup>
+import { useUserStore } from '@/stores/userStore'
 import { ref } from 'vue'
 
 // TODO: 根据路由参数获取食谱详情
@@ -35,6 +36,59 @@ const goCollect = () => {
   // TODO: 收藏逻辑
   console.log('点击收藏')
 }
+
+// 菜谱评论
+const commentList = ref([
+  {
+    id: 1,
+    username: '小红',
+    editTime: '2024-06-27',
+    content: '这个菜谱很好吃！'
+  },
+  {
+    id: 2,
+    username: '小刚',
+    editTime: '2024-06-28',
+    content: '我也觉得！'
+  }
+])
+
+// 动作面板逻辑
+const userStore = useUserStore()
+const token = userStore.userInfo.token
+
+const showAction = ref(false)
+const formRef = ref(null)
+const ruleForm = ref({
+  title: '',
+  content: ''
+})
+const rules = ref({
+  title: [{ required: true, message: '请输入标题', trigger: 'onBlur' }],
+  content: [{ required: true, message: '请输入评论内容', trigger: 'onBlur' }]
+})
+/**
+ * @description: 提交评论
+ * @return {*}
+ * @example: 例子
+ */
+const onSubmit = () => {
+  console.log(ruleForm.value)
+  // TODO: 提交评论
+}
+/**
+ * @description: 重置表单
+ * @return {*}
+ * @example: 例子
+ */
+const clearForm = () => {
+  if (!token) return
+
+  ruleForm.value.title = ''
+  ruleForm.value.content = ''
+  formRef.value.resetValidation()
+  showAction.value = false
+}
 </script>
 <template>
   <div class="container">
@@ -50,7 +104,6 @@ const goCollect = () => {
         <van-icon name="wap-home-o" size="30" color="#000" />
       </template>
     </van-nav-bar>
-
     <div class="content">
       <van-image
         v-if="cookObj.img"
@@ -58,7 +111,6 @@ const goCollect = () => {
         height="200"
         :src="cookObj.img"
       />
-
       <div class="detail">
         <h1>{{ cookObj.title }}</h1>
         <div class="msg">
@@ -69,7 +121,6 @@ const goCollect = () => {
           点击收藏
         </van-button>
       </div>
-
       <p class="title">食材准备</p>
       <van-cell-group inset>
         <van-cell
@@ -79,11 +130,67 @@ const goCollect = () => {
           :value="item.quantity"
         />
       </van-cell-group>
-
       <p class="title">制作步骤</p>
       <van-cell-group inset>
         <van-cell :value="cookObj.content" />
       </van-cell-group>
+
+      <div class="comment">
+        <van-cell-group>
+          <van-cell title="评论区" style="color: #1c8eff">
+            <template #right-icon>
+              <van-icon
+                name="add"
+                size="25"
+                color="#1c8eff"
+                @click="showAction = true"
+              />
+            </template>
+          </van-cell>
+        </van-cell-group>
+
+        <van-action-sheet
+          v-model:show="showAction"
+          title="添加评论"
+          style="padding: 5px"
+          @close="clearForm"
+        >
+          <van-form @submit="onSubmit" ref="formRef" v-if="token">
+            <van-field
+              v-model="ruleForm.title"
+              name="评论标题"
+              label="评论标题"
+              placeholder="请输入评论标题"
+              :rules="rules.title"
+              required
+            />
+            <van-field
+              v-model="ruleForm.content"
+              rows="2"
+              autosize
+              label="评论内容"
+              type="textarea"
+              :rules="rules.content"
+              placeholder="请输入评论内容"
+              required
+            />
+            <van-button round block type="primary" native-type="submit">
+              提交评论
+            </van-button>
+          </van-form>
+          <van-empty v-else description="请先登录" />
+        </van-action-sheet>
+
+        <van-cell-group>
+          <van-cell
+            v-for="item in commentList"
+            :key="item.id"
+            :title="item.username"
+            :label="item.editTime"
+            :value="item.content"
+          />
+        </van-cell-group>
+      </div>
     </div>
   </div>
 </template>
@@ -117,6 +224,9 @@ const goCollect = () => {
         font-size: 14px;
         margin-top: 10px;
       }
+    }
+    .comment {
+      margin-top: 20px;
     }
   }
 }
